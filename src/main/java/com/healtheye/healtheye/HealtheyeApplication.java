05332fe107c9f0031d59f40ae334f6071ad6e7d9
Damngz <da.munozgz@gmail.com>
2025-01-27 17:38:43 -0300

    base project

---
+++ src/main/java/com/healtheye/healtheye/HealtheyeApplication.java
@@ -0,0 +1,13 @@
+package com.healtheye.healtheye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealtheyeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealtheyeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/healtheye/healtheye/HealtheyeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healtheye.healtheye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealtheyeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
